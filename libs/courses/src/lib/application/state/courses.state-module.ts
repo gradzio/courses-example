a05f9f15b5444701819add5d8bad30f09b8bc167
gradzio <gregradzio@gmail.com>
2022-02-03 08:23:50 +0100

    fix(courses): typos after re-generating

--- libs/courses/src/lib/application/state/courses.state-module.ts
+++ libs/courses/src/lib/application/state/courses.state-module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { GETS_COURSE_QUERY } from '../ports/primary/gets-course-query.port';
 import { CoursesState } from './courses.state';
-import { CREATES_COURSE_COMMAND } from '../ports/primary/creates-course-command.port';
-import { DELETES_COURSE_COMMAND } from '../ports/primary/deletes-course-command.port';
+import { CREATE_COURSE_COMMAND } from '../ports/primary/create-course-command.port';
+import { DELETE_COURSE_COMMAND } from '../ports/primary/delete-course-command.port';
 
 @NgModule({
   declarations: [],
   providers: [
     CoursesState,
     { provide: GETS_COURSE_QUERY, useExisting: CoursesState },
-    { provide: CREATES_COURSE_COMMAND, useExisting: CoursesState },
-    { provide: DELETES_COURSE_COMMAND, useExisting: CoursesState }
+    { provide: CREATE_COURSE_COMMAND, useExisting: CoursesState },
+    { provide: DELETE_COURSE_COMMAND, useExisting: CoursesState }
   ],
   exports: [],
 })
